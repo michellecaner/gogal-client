4e43d9ebdc4b881e4fdc2467410774a2eea773f9
michellecaner <michellecaner@gmail.com>
2022-09-10 12:46:57 -0500

    setting up Trip Manager

--- src/components/trip/TripCard.js
+++ src/components/trip/TripCard.js
@@ -2,13 +2,17 @@ import React from "react"
 import { Link } from "react-router-dom"
 import "./Trip.css"
 
-export const TripCard = ({trip}) => {
-
-  return(
-<section className="trip">
-    <h3 className="trip__title">Turkey 2019</h3>
-    <button>Let's Go!</button>
-  </section>
+export const TripCard = ({ trip }) => {
+  return (
+    <div className="card">
+    <div className="card-content">
+      <h3><span className="trip-title">{trip.title}
+      </span></h3>
+      <Link to={`/trips/${trip.id}`}>
+        <button type="button" className="lets-go-btn">Let's Go!</button>
+      </Link>
+      {/* <button type="button" className="delete-btn" onClick={() => handleDeleteProject(trip.id)}>Delete</button> */}
+    </div>
+</div>  
   )
-  
 }
