9a01c57498efd5741d02e92cf91c3df2f72efe0a
michellecaner <michellecaner@gmail.com>
2022-09-12 19:34:29 -0500

    basic form fields rendering

--- src/components/trip/TripForm.js
+++ src/components/trip/TripForm.js
@@ -39,7 +39,85 @@ export const TripForm = () => {
     <form className="tripForm">
       <h2 className="tripForm__title">Create New Trip</h2>
       <fieldset>
-
+        <div className="form-group">
+          <label htmlFor="title">Title: </label>
+          <input type="text" name="title" required autoFocus className="form-control"
+            defaultValue={currentTrip.title}
+            onChange={changeTripState}
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="image_url_one">Image URL #1: </label>
+          <input type="text" name="image_url_one" required autoFocus className="form-control"
+            value={currentTrip.image_url_one}
+            onChange={changeTripState}
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="image_url_two">Image URL #2: </label>
+          <input type="text" name="image_url_two" required autoFocus className="form-control"
+            value={currentTrip.image_url_two}
+            onChange={changeTripState}
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="image_url_three">Image URL #1: </label>
+          <input type="text" name="image_url_three" required autoFocus className="form-control"
+            value={currentTrip.image_url_three}
+            onChange={changeTripState}
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="country">Country: </label>
+          <input type="text" name="country" required autoFocus className="form-control"
+            value={currentTrip.country}
+            onChange={changeTripState}
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="city">City: </label>
+          <input type="text" name="city" required autoFocus className="form-control"
+            value={currentTrip.city}
+            onChange={changeTripState}
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="from_date">From Date: </label>
+          <input type="date" name="from_date" required autoFocus className="form-control"
+            value={currentTrip.from_date}
+            onChange={changeTripState}
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="to_date">To Date: </label>
+          <input type="date" name="to_date" required autoFocus className="form-control"
+            value={currentTrip.to_date}
+            onChange={changeTripState}
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="content">Content: </label>
+          <textarea rows="10" name="content" required autoFocus className="form-control"
+            value={currentTrip.content}
+            onChange={changeTripState}
+          />
+        </div>
       </fieldset>
     </form>
   )
