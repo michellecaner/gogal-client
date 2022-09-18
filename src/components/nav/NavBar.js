4e360117ab5c6c99b42e0edc20f6dec4d887050e
michellecaner <michellecaner@gmail.com>
2022-09-18 18:06:25 -0500

    responsive nav bar working

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -6,6 +6,8 @@ export const NavBar = () => {
 
     const history = useHistory()
 
+    const [isNavExpanded, setIsNavExpanded] = useState(false)
+
     return (
         <nav className="navbar">
             <div className="navbar__title">
@@ -13,44 +15,61 @@ export const NavBar = () => {
                     <Link className="navbar__link" to="/">go gal!</Link>
                 </li>
             </div>
-            <a href="#" className="toggle__button">
-                    <span className="bar"></span>
-                    <span className="bar"></span>
-                    <span className="bar"></span>
-            </a>
-            <ul className="navbar__menu">
-                <li className="navbar__item">
-                    <Link className="navbar__link" to="/traveltips">travel tips</Link>
-                </li>
-                <li className="navbar__item">
-                    <Link className="navbar__link" to="/trips">my trips</Link>
-                </li>
-                <li className="navbar__item">
-                    <Link className="navbar__link" to="/categories">categories</Link>
-                </li>
-                <li className="navbar__item">
-                    <Link className="navbar__link" to="/mygogals">my go gals</Link>
-                </li>
-                {
-                    (localStorage.getItem("gg_token") !== null) ?
-                        <li className="navbar__item">
-                            <Link className="navbar__link fakeLink"
-                                onClick={() => {
-                                    localStorage.removeItem("gg_token")
-                                    history.push({ pathname: "/" })
-                                }}
-                            >log out</Link>
-                        </li> :
-                        <>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/login">Login</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/register">Register</Link>
-                            </li>
-                        </>
+            <button className="hamburger"
+                onClick={() => {
+                    setIsNavExpanded(!isNavExpanded);
+                }}>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="white"
+                >
+                    <path
+                        fillRule="evenodd"
+                        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                        clipRule="evenodd"
+                    />
+                </svg>
+            </button>
+            <div className={
+                isNavExpanded ? "navigation__menu expanded" : "navigation__menu"
             }
-            </ul>
+            >
+                <ul className="navbar__menu">
+                    <li className="navbar__item">
+                        <Link className="navbar__link" to="/traveltips">travel tips</Link>
+                    </li>
+                    <li className="navbar__item">
+                        <Link className="navbar__link" to="/trips">my trips</Link>
+                    </li>
+                    <li className="navbar__item">
+                        <Link className="navbar__link" to="/categories">categories</Link>
+                    </li>
+                    <li className="navbar__item">
+                        <Link className="navbar__link" to="/mygogals">my go gals</Link>
+                    </li>
+                    {
+                        (localStorage.getItem("gg_token") !== null) ?
+                            <li className="navbar__item">
+                                <Link className="navbar__link fakeLink"
+                                    onClick={() => {
+                                        localStorage.removeItem("gg_token")
+                                        history.push({ pathname: "/" })
+                                    }}
+                                >log out</Link>
+                            </li> :
+                            <>
+                                <li className="nav__item">
+                                    <Link className="nav__link" to="/login">Login</Link>
+                                </li>
+                                <li className="nav__item">
+                                    <Link className="nav__link" to="/register">Register</Link>
+                                </li>
+                            </>
+                    }
+                </ul>
+            </div>
         </nav>
     )
 }
