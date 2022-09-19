1def0a34f1dc94036ba14ffa8d1dee695b7462f9
michellecaner <michellecaner@gmail.com>
2022-09-18 22:37:19 -0500

    responsive tip cards

--- src/components/tip/TipCard.js
+++ src/components/tip/TipCard.js
@@ -5,7 +5,7 @@ export const TipCard = ({ tip }) => {
   return (
     <div className="card">
       <div className="card__title">go gal!trip tip</div>
-      <div className="card__content">{tip.tip}</div>
+      <p className="card__content">{tip.tip}</p>
     </div>
   );
 }
